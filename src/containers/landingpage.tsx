c90ef6e1b0a4aaaa3d7efd716a3af14d72b0c126
odin5on <108775964+odin5on@users.noreply.github.com>
2023-10-30 19:36:24 -0500

    feat: temporary landing page

---
+++ src/containers/landingpage.tsx
@@ -0,0 +1,16 @@
+const LandingPage = () => {
+  return (
+    <div className="bg-primary flex justify-center">
+      {/* Your app content goes here */}
+      <div className="h-screen w-5/6 max-w-5xl border-2 flex flex-col">
+        <div className="h-24 border-b-2 bg-secondary">
+
+        </div>
+        
+        <p>hello world</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// import colors from 'tailwindcss/colors';
+
+export const content = ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'];
+export const theme = {
+  extend: {
+    colors: {
+      'primary': '#FCF3E0',
+      'secondary': '#ECBB38',
+      'accent': '#67D6AB',
+    }
+  },
+};
+export const plugins = [];
