59e5e697ce12a5a6d7737c73d8e132afe8519f25
odin5on <108775964+odin5on@users.noreply.github.com>
2024-01-27 14:55:23 -0600

    feat: textured background + tab logo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import LandingPage from "./containers/landingpage";
 import About from "./containers/about";
 import Projects from "./containers/projects";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import noise from "./assets/images/noise-light.png";
 import { ThemeContext } from "./contexts/theme-context";
 
 const routes = [
@@ -59,7 +59,7 @@ const App = () => {
 
   return (
     <ThemeContext.Provider value={{ theme, handleThemeChange }}>
-      <div className="relative flex h-full min-h-screen w-full flex-col border-dark bg-light text-dark dark:border-light dark:bg-dark dark:text-light overscroll-contain">
+      <div className="relative flex h-full min-h-screen w-full flex-col border-dark bg-light text-dark dark:border-light dark:bg-dark dark:text-light overscroll-contain" style={{ backgroundImage: `url(${noise})` }}>
         <RouterProvider router={router} />
       </div>
     </ThemeContext.Provider>
